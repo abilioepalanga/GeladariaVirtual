b74f8eba89fca1ebcae36129a2b1a163fdddacff
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2024-08-27 21:04:13 +0100

    Alterando os left 1 e 2

---
+++ main.js
@@ -0,0 +1,17 @@
+/*-------Toggle Button_marcador de seção--*/
+const navMenu = document.getElementById("nav-menu"),
+    navLink = document.querySelectorAll(".nav_link"),
+    hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("left-[0]");
+});
+/*-------Show Scroll Up_exibir apos rolar a página--*/
+
+/*-------Change Background Header_alterar o fundo header--*/
+
+/*-------Swiper____criar sliders------------*/
+
+/*------Scrool Sections Active Link_link ativo ao rolar a página--*/
+
+/*------Scroll Reveall Animation_animação ao rolar a página--*/
