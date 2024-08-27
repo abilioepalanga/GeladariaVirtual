55dd2c70cf709b865ac19339e44076770578cd3e
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2024-08-27 16:43:22 +0100

    Redimensionando o container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
     content: ["./*.{html,js}"],
     theme: {
+        screens: {
+            sm: "340px",
+            md: "540px",
+            lg: "768px",
+            xl: "1180px",
+        },
         extend: {},
     },
     plugins: [],
